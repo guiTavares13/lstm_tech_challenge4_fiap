bd914481e86c210a7fd2bc9c111dcc0404653d18
Guilherme Tavares <t794746@f1rst.com.br>
2024-11-27 21:58:34 -0300

    Incluindo os codigos do projeto

--- extract_data_b3.py
+++ extract_data_b3.py
@@ -13,15 +13,22 @@ class ExtractDataB3():
     return df
 
   def save_data(self, df):
-    csv_data = df.to_csv('out.csv')
-      
+    csv_data = df.to_csv(index=False)
+    try:
+      mounts = dbutils.fs.ls('/mnt/streaming-avro/tech_challenge/extracao/')
+    except Exception as e:
+      print(f'Erro: Montagem não encontrada - {e}')
+      return
+    dbutils.fs.put(
+      f'/mnt/streaming-avro/tech_challenge/extracao/{self.end_date.strftime("%Y%m%d")}/extracao_lote_{self.start_date}_{self.end_date.strftime("%Y-%m-%d")}.csv', 
+      csv_data
+    )
 
-
-#date_parameter = sys.argv[1].split("T")[0]
-date_parameter = '2024-06-11'
+date_parameter = sys.argv[1].split("T")[0]
+#date_parameter = '2024-11-18'
 end_date = datetime.strptime(date_parameter, '%Y-%m-%d')
-start_date_temp = end_date - timedelta(days=628)
-start_date = start_date_temp.strftime('%Y-%m-%d')
+seven_days_before = end_date - timedelta(days=730)
+start_date = seven_days_before.strftime('%Y-%m-%d')
 
 try:
   extract_data_b3 = ExtractDataB3(start_date, end_date)
